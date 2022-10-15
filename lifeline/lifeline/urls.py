5e8aae8f4d00012bc7d5691093bdfe15ac17202f
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2022-10-15 13:01:26 +0530

    added home page

--- lifeline/lifeline/urls.py
+++ lifeline/lifeline/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blood_donation.urls')),
+    path("",include('blood_donation.urls')),
 ]
